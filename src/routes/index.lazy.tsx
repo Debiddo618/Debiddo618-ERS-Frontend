2133d35d2e317dc5afa92111a93eb05cfd9c862d
Debiddo <dzheng@terpmail.umd.edu>
2024-12-23 16:43:54 -0500

    setup tailwind

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -8,6 +8,7 @@ function Index() {
     return (
         <div className="p-2">
             <h3>Welcome Home!</h3>
+            <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
         </div>
     )
 }
