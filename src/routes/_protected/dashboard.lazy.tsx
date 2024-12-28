2aa2e216f92b579b4fc5147bc016db5ab59fd674
Debiddo <dzheng@terpmail.umd.edu>
2024-12-28 14:21:48 -0500

    hide the dashboard if create reimbursement form is showing

--- src/routes/_protected/dashboard.lazy.tsx
+++ src/routes/_protected/dashboard.lazy.tsx
@@ -1,14 +1,11 @@
 import { useContext, useEffect, useState } from 'react';
 import { getUser, User } from '@/lib/authUtils';
-import axiosInstance from '@/lib/axios-config';
-import { useQuery } from '@tanstack/react-query';
 import { createLazyFileRoute } from '@tanstack/react-router';
-import Reimbursement from '@/components/Reimbursement';
 import ReimbursementEditForm from '@/components/Reimbursement-editform';
 import ReimbursementList from '@/components/Reimbursement-list';
 import { useFetchRole } from '@/hooks/use-fetchRole';
-import { useFetchReimbursementByUser } from '@/hooks/use-fetchReimbursementByUser';
 import { ReimbursementTable } from '@/components/Reimbursement-table';
+import { ShowFormContext } from '../_protected';
 
 export const Route = createLazyFileRoute('/_protected/dashboard')({
   component: RouteComponent,
@@ -24,6 +21,8 @@ type Reimbursements = {
 function RouteComponent() {
   const [user, setUser] = useState<User | null>(null);
   const [selected, setSelected] = useState<Reimbursements | null>(null);
+  const show = useContext(ShowFormContext);
+
 
   useEffect(() => {
     const currentUser = getUser();
@@ -34,7 +33,7 @@ function RouteComponent() {
 
   return (
     <div className="">
-      {!selected && (
+      {(!selected && !show) && (
         <div className="">
           {role === "MANAGER" ? <ReimbursementTable /> : null}
           <div className='flex justify-evenly p-4 w-screen backdrop-blur-lg bg-white/30'>
