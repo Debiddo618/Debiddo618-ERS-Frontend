cf31b6fab236a1cd07641cb8f2a2222a56bae773
Debiddo <dzheng@terpmail.umd.edu>
2024-12-31 01:47:27 -0500

    changed axios url for testing

--- src/lib/axios-config.ts
+++ src/lib/axios-config.ts
@@ -3,7 +3,7 @@ import { jwtDecode } from "jwt-decode";
 
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "/api",
     headers: {
         "Content-Type": "application/json",
     }
